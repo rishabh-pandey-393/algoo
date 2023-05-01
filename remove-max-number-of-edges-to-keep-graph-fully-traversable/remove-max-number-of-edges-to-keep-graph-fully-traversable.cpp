a82d4215cc8ecbf74e6d7cdf2aa2643a31a9d65f
rishabh-pandey-393 <81481527+rishabh-pandey-393@users.noreply.github.com>
2023-05-02 03:15:21 +0530

    Time: 1098 ms (5.02%) | Memory: 154.1 MB (30.43%) - LeetSync

--- remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-    int findParent(vector<int>& parent, int u) {
-        if(parent[u] < 0)
-            return u;
-        return parent[u] = findParent(parent, parent[u]);
+    int find(int a,vector<int>&par){
+        if(a==par[a]) return a;
+        return par[a]=find(par[a],par);
     }
-    bool UnionByWeight(vector<int>& parent, int u, int v) {
-        int pu = findParent(parent, u), pv = findParent(parent, v);
-        if(pu == pv)
-            return false;
-        if(parent[pu] < parent[pv]) {
-            parent[pu] += parent[pv];
-            parent[pv] = pu;
-        }
-        else {
-            parent[pv] += parent[pu];
-            parent[pu] = pv;
-        }
-        return true;
+    void unionn(int a,int b,vector<int>&par){
+        a=find(a,par),b=find(b,par);
+        par[a]=b;
     }
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        vector<int> parent(n + 1, -1);
-        int remove = 0;
-        for(vector<int>& edge: edges)
-            if(edge[0] == 3 && !UnionByWeight(parent, edge[1], edge[2]))
-                    remove++;
-        vector<int> a(parent), b(parent);
-        for(vector<int>& edge: edges) {
-            if(edge[0] == 1 && !UnionByWeight(a, edge[1], edge[2]))
-                    remove++;
-            else if(edge[0] == 2 && !UnionByWeight(b, edge[1], edge[2]))
-                    remove++;
+        int res=0,cta=1,ctb=1;
+        vector<int>para(n+1,0),parb(n+1,0);
+        for(int i=0;i<n+1;i++){
+            para[i]=i;
+            parb[i]=i;
         }
-        int groupa = 0, groupb = 0;
-        for(int i = 1; i <= n; i++) {
-            if(a[i] < 0)
-                groupa++;
-            if(b[i] < 0)
-                groupb++;
+        sort(edges.rbegin(),edges.rend());
+        for(auto e:edges){
+            if(e[0]==3){
+                if(find(e[1],para)!=find(e[2],para)) {
+                    unionn(e[1],e[2],para);
+                    unionn(e[1],e[2],parb);
+                    cta++;ctb++;
+                }
+                else res++;
+            }
+            else if(e[0]==2){
+                if(find(e[1],parb)!=find(e[2],parb)) {
+                    unionn(e[1],e[2],parb);
+                    ctb++;
+                }
+                else res++;
+            }
+            else{
+                if(find(e[1],para)!=find(e[2],para)) {
+                    unionn(e[1],e[2],para);
+                    cta++;
+                }
+                else res++;
+            }
         }
-        if(groupa > 1 || groupb > 1)
-            return -1;
-        return remove;
+        cout<<cta<<" "<<ctb<<endl;
+        if(cta!=n or ctb!=n) return -1;
+        return res;
     }
 };
